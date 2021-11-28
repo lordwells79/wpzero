5adea4d588106a28814794ef6d1959cfdba40eb9
Carlo Luigi Rizzello <luigi@MacBook-Air-di-Carlo.local>
2021-11-28 16:34:08 +0100

    add bootstrap-functions-gitignore

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+/*-----------------------------------------------------------------------------------*/
+/* Enqueue script and styles */
+/*-----------------------------------------------------------------------------------*/
+
+
+if ( ! function_exists('wpzero_enqueue_scripts') ) {
+    function wpzero_enqueue_scripts() {
+        wp_enqueue_style(
+            'bootstrap-grid',
+            get_template_directory_uri() . '/css/bootstrap-grid.css',
+            array(),'4.6.1'
+        );
+    }
+    add_action( 'wp_enqueue_scripts', 'wpzero_enqueue_scripts' );
+}
+
+
+/*-----------------------------------------------------------------------------------*/
+/* Setup theme */
+/*-----------------------------------------------------------------------------------*/
+
+if (!function_exists('wpzero_after_setup_theme')) {
+
+    function wpzero_after_setup_theme() {
+
+        global $content_width;
+
+        if( ! isset ($content_width) ) $content_width = 1100;
+
+        add_theme_support('title_tag');
+        add_theme_support('automatic-feed-links');
+
+    }
+    add_action( 'after_setup_theme', 'wpzero_after_setup_theme' );
+}
