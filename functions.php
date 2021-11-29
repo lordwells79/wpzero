5efa3572dd1606636152b431c4c7ab2cb47111e4
Luigi <lordwells79@hotmail.it>
2021-11-29 16:00:45 +0100

    Caricato font e navigation.js

--- functions.php
+++ functions.php
@@ -7,12 +7,27 @@
 if ( ! function_exists('wpzero_enqueue_scripts') ) {
     function wpzero_enqueue_scripts() {
         wp_enqueue_style('wpzero-style', get_stylesheet_uri(), array() );
-        
+
+        $googleFontsArgs = array(
+			'family' =>	str_replace('|', '%7C','PT+Serif:700|Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i'),
+			'subset' =>	'latin,latin-ext'
+		);
+
+		wp_enqueue_style('google-fonts', add_query_arg ( $googleFontsArgs, "https://fonts.googleapis.com/css" ), array(), '1.0.0' );
+
         wp_enqueue_style(
             'bootstrap-grid',
             get_template_directory_uri() . '/css/bootstrap-grid.css',
             array(),'4.6.1'
         );
+
+        wp_enqueue_script(
+			'wpzero-navigation',
+			get_template_directory_uri() . '/js/navigation.js',
+			FALSE,
+			'1.0',
+			TRUE
+		);
     }
     add_action( 'wp_enqueue_scripts', 'wpzero_enqueue_scripts' );
 }
